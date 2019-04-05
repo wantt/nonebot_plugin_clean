624c2ea21090e0d72e3b683bc351f2c1c178ff54
wantt <wantt2@qq.com>
2019-04-05 13:42:11 +0800

    add file

---
+++ awsome-bot/awesome/plugins/adchatterbot.py
@@ -0,0 +1,113 @@
+import nonebot
+from nonebot import permission as perm
+import pytz
+from aiocqhttp.exceptions import Error as CQHttpError
+from nonebot import on_command, CommandSession
+from nonebot import on_natural_language, NLPSession, NLPResult
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import chatterbot
+import json
+import re
+
+
+if 1:
+    chatbot = ChatBot("seven",
+            logic_adapters=[{'import_path':'chatterbot.logic.BestMatch','default_response':'#','maximum_similarity_threshold':0.90}],
+            read_only=True)
+    trainer = ListTrainer(chatbot)
+    trainer.export_for_training('./my_export.json')
+
+
+ad_private_message = '你好，我出校园网'
+@on_natural_language(only_to_me=False)
+async def _(session:NLPSession):
+    rule=r'([收买求有租(有.*转让)].*[网(创翼)])(?![球拍站吧友速上址银页名])'
+    if re.match(rule,session.msg):
+        await session.send('你好，我出校园网',ensure_private = True)
+    elif session.ctx['message_type']=='private':
+        response_ad= chatbot.get_response(session.msg)
+        if not ("#" in str(response_ad)):
+            await session.send(f"{chatbot.get_response(session.msg)}")
+        
+#    elif '大家好' in session.msg:
+#        await session.send('哦')
+#    else:
+#        grouplist=await session.bot.get_group_list()
+#        if session.ctx['message_type'] == 'group':
+##            print(session.ctx['user_id'])
+#            print(session.ctx['group_id'])
+#            if 1:#session.ctx['group_id'] == int('251347617'):
+#                await session.send(message = 'hello',ensure_private = True)
+#        elif session.ctx['message_type']=='discuss':
+#            print(f"{session.ctx['discuss_id']}")
+#    await    bot.send_private_msg(user_id=int(2188156040),message='123')
+#    await bot.send_private_msg(user_id=int(1048517841),message='123')
+#        await session.send(f"{chatbot.get_response(session.msg)}")
+
+
+@on_command('gai_pri',aliases='k',only_to_me = True,permission=perm.SUPERUSER)
+async def drop_bot(session:CommandSession):
+    if (session.current_arg_text):
+        print(session.current_arg_text)
+        ad_private_message = session.current_arg_text
+#    session.current_arg_text.split("#")
+
+    
+@on_command('drop_bot',aliases='d',only_to_me = True,permission=perm.SUPERUSER)
+async def drop_bot(session:CommandSession):
+    chatbot.storage.drop()
+    await session.send("success drop")
+
+@on_command('check_conv',aliases='x',only_to_me = True,permission=perm.SUPERUSER)
+async def check_conv(session:CommandSession):
+    all_conv = []
+    num=0
+    with open('./my_export.json','r') as f:
+        load_dict = json.load(f)
+        for k in load_dict.keys():
+            for i in load_dict[k]:
+                tmpconv = "#".join(i)
+                all_conv.append(str(num)+'. ' + tmpconv)
+                num +=1
+        await session.send('\n\n'.join(all_conv))
+        
+'''
+@on_command('gai_conv',aliases='g',only_to_me = True)
+async def gai_conv(session:CommandSession):
+    all_conv = []
+    num=0
+    with open('./my_export.json','r') as f:
+        load_dict = json.load(f)
+        for k in load_dict.keys():
+            for i in load_dict[k]:
+                tmpconv = "#".join(i)
+                all_conv.append(str(num) + '  ' + tmpconv)
+                num +=1
+        await session.send("\n\n".join(all_conv))
+        change_num = session.get('change_num',prompt="输入你要修改或者删除的数字：")
+        print(change_num)
+    if change_num.isdigit() and int(change_num) < num:
+        del load_dict['conversations'][int(change_num)]
+        with open('./my_export.json','w+') as f:
+            json.dump(load_dict,f)
+            chatbot.storage.drop()
+            trainer.train(load_dict['conversations'])
+
+        await session.send(load_dict['conversations'])
+'''
+
+@on_command('learn',aliases='l',only_to_me = True,permission=perm.SUPERUSER)
+async def learn(session:CommandSession):
+    list_text = session.current_arg_text.split('#')
+    tmpconversation=[]
+    if len(list_text) ==2:
+        for con in list_text:
+            tmpconversation.append(con)
+        trainer.train(tmpconversation)
+        await session.send("成功")
+        trainer.export_for_training('./my_export.json')
+    else:
+        await session.send("格式错误，正确格式：l 问题#答案")
+        
+        
---
+++ awsome-bot/awesome/plugins/adscheduler.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+import nonebot
+import pytz
+from aiocqhttp.exceptions import Error as CQHttpError
+from nonebot import on_command, CommandSession
+from aiofile import AIOFile
+import random
+from nonebot import permission as perm
+
+ad_message=['便宜出一个校园网，最好长期','转让一个校园网，最好长期','出一个校园网，需要的私聊','出一个校网，需要的同学请私聊我','便宜出一个校园网啦，要的私聊','出一个校园网，有人需要吗，私聊']
+
+#@nonebot.scheduler.scheduled_job('cron',hour='10',minute='*')
+#@nonebot.scheduler('cron',hour='10',minute='*')
+@nonebot.scheduler.scheduled_job('cron',hour='*/8',minute='0')
+async def _():
+    bot = nonebot.get_bot()
+    now  = datetime.now(pytz.timezone('Asia/Shanghai'))
+    try:
+        adgroup=[]
+        
+        with open('./adgroup.txt','r') as f:
+            for group_id in f.read().split('\n')[:-1]:  # [531481587,161631645]:
+                groupid = group_id.split('#')[0]
+                if groupid not in adgroup and groupid.isdigit():
+                    adgroup.append(groupid)
+                    print(groupid)
+                    admessage = group_id.split('#')[1:]
+#                    await bot.send_group_msg(group_id=int(groupid), message=f'{"".join(admessage)}')
+                    await bot.send_group_msg(group_id=int(groupid), message=f'{"".join(random.choice(ad_message))}')
+#                    await session.send(f'{group_id}定时发送成功')
+       
+    except CQHttpError:
+        pass
+#        await session.send('定时发送失败')
+     
+
+@on_command('add_adgroup',aliases=("+"),only_to_me = True,permission=perm.SUPERUSER)
+async def add_adgroup(session:CommandSession):
+#    if len(session.current_arg_text.split('#')) == 1:
+#        admessage = session.current_arg_text.split('#')[0]+'#'+ad_message 
+#    else:
+#        admessage = session.current_arg_text
+        
+
+    if session.current_arg_text.split('#')[0].isdigit():
+        async with AIOFile('./adgroup.txt','a+') as f:
+            #await f.write(admessage +'\n')
+            await f.write(session.current_arg_text.split('#')[0] +'\n')
+            await f.fsync()
+        await session.send(f'添加成功 {admessage}')
+    else:
+        await session.send("添加失败，正确格式：+ 群号#内容 \n 如：+ 1122341")
+
+        
+        
+        
+@on_command('del_adgroup',aliases=("-"),only_to_me=True,permission=perm.SUPERUSER)
+async def del_adgroup(session:CommandSession):
+    print(session.current_arg_text)
+    adgroups = []
+    with open('./adgroup.txt','r') as f:
+        for content in f.read().split('\n')[:-1]:
+            group_id = content.split('#')[0]
+            if content not in adgroups and group_id.isdigit():
+                adgroups.append(content)
+    if session.current_arg_text.split('#')[0].isdigit() and session.current_arg_text.split('#')[0] in adgroups:
+        adgroups.remove(session.current_arg_text)
+        await session.send(f'删除成功{session.current_arg_text}')
+    else:
+        await session.send(f'删除失败，不存在这条群发 {session.current_arg_text}')
+
+    with open('./adgroup.txt','w+') as f:
+        for group_id in adgroups:
+            f.write(group_id + '\n')
+    
+
+@on_command('modifyDefaultMsg',aliases=('m'),only_to_me=True,permission=perm.SUPERUSER)
+async def modifyDefaultMsg(session:CommandSession):
+    ad_message = session.current_arg_text
+    session.send(f'修改默认群发成功，现在的默认群发：{ad_message}')
+    
+@on_command('send_ad',aliases=('s'),only_to_me=True,permission=perm.SUPERUSER)
+async def send_ad(session:CommandSession):
+    bot = nonebot.get_bot()
+    if 1:
+        adgroup=[]
+        with open('./adgroup.txt','r') as f:
+            for group_id in f.read().split('\n')[:-1]:  # [531481587,161631645]:
+                try:
+                    groupid = group_id.split('#')[0]
+                    if groupid not in adgroup and groupid.isdigit():
+                        adgroup.append(groupid)
+                        await bot.send_group_msg(group_id=int(groupid), message=f'{"".join(random.choice(ad_message))}')
+#                        await bot.send_group_msg(group_id=int(groupid), message=f'{"".join(admessage)}')
+                        await session.send(f'群{groupid}成功发送消息成功{admessage}')
+       
+                except CQHttpError:
+                    await session.send(f'{groupid} 发送失败,你应该没有这个群')
+@on_command('check_ad',aliases='c',only_to_me=True,permission=perm.SUPERUSER)
+async def check_ad(session:CommandSession):
+    adgroup= []
+    with open('./adgroup.txt','r') as f:
+        for message in f.read().split("\n")[:-1]:
+            adgroup.append(message+'\n')
+    await session.send(f'{"".join(adgroup)}')
+@on_command('adhelp',aliases='h',only_to_me=True,permission=perm.SUPERUSER)
+async def adhelp(session:CommandSession):
+    await session.send(f'+: 添加群发,+ 123#出一个校园网  \n -:删除群发，- 123#出一个校园网 \n s:发送群发 s\n c: 查看群发 c \n m:修改默认群发 m 出一个校园网\n h:显示本帮主信息')
+#@on_command()
